0d14763c52c33a1c7399d6f3fd8cbe403c6d0e8d
Phil Wang <lucidrains@gmail.com>
2022-11-26 11:31:11 -0800

    make sure equivariance is tested for mlp attention

--- tests/test_equivariance.py
+++ tests/test_equivariance.py
@@ -19,10 +19,15 @@ def test_transformer():
     type0, _ = model(feats, coors, mask)
     assert type0.shape == (1, 32, DIM), 'output must be of the right shape'
 
-def test_equivariance():
+@pytest.mark.parametrize('dot_product_attention', [True, False])
+def test_equivariance(
+    dot_product_attention
+):
+
     model = Equiformer(
         dim = DIM,
         depth = 1,
+        dot_product_attention = dot_product_attention,
         reduce_dim_out = True
     )
 
