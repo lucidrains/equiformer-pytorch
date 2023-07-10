fix rotation matrix to euler angle function, tests now pass

equiformer_pytorch/irr_repr.py
@@ -30,6 +30,9 @@ def exists(val):
 def identity(t):
     return t
 
+def l2norm(t):
+    return F.normalize(t, p = 2, dim = -1)
+
 def pack_one(t, pattern):
     return pack([t], pattern)
 
@@ -155,17 +158,21 @@ def rot_to_euler_angles(R):
     Rotation matrix to ZYZ Euler angles
     '''
     device, dtype = R.device, R.dtype
-    xyz = R @ torch.tensor([0, 0, 1.], device = device, dtype = dtype)
-    a, b = x_to_alpha_beta(xyz)
-    rotz = rot(0, -b, -a) @ R
-    c = atan2(rotz[..., 1, 0], rotz[..., 0, 0])
+    xyz = R @ torch.tensor([0.0, 1.0, 0.0], device = device, dtype = dtype)
+    xyz = l2norm(xyz).clamp(-1., 1.)
+
+    b = acos(xyz[..., 1])
+    a = atan2(xyz[..., 0], xyz[..., 2])
+
+    R = rot(a, b, torch.zeros_like(a)).transpose(-1, -2) @ R
+    c = atan2(R[..., 0, 2], R[..., 0, 0])
     return torch.stack((a, b, c), dim = -1)
 
 def rot_x_to_y_direction(x, y):
     '''
     Rotates a vector x to the same direction as vector y
     Taken from https://math.stackexchange.com/a/2672702
-    Turns out there are multiple rotation matrices that can bring one vector to another. Re-examine this taken solution, could be why nothing is working
+    This formulation, although not the shortest path, has the benefit of rotation matrix being symmetric; rotating back to x upon two rotations
     '''
     n, dtype, device = x.shape[-1], x.dtype, x.device
 
@@ -176,8 +183,7 @@ def rot_x_to_y_direction(x, y):
 
     x, y = x.double(), y.double()
 
-    x = F.normalize(x, dim = -1)
-    y = F.normalize(y, dim = -1)
+    x, y = map(l2norm, (x, y))
 
     xy = rearrange(x + y, '... n -> ... n 1')
     xy_t = rearrange(xy, '... n 1 -> ... 1 n')
@@ -190,26 +196,11 @@ def compose(a1, b1, c1, a2, b2, c2):
     (a, b, c) = (a1, b1, c1) composed with (a2, b2, c2)
     """
     comp = rot(a1, b1, c1) @ rot(a2, b2, c2)
-    return rot_to_euler_angles(comp).unbind(dim = -1)
-
-def compose_tensor(angles1, angles2):
-    angles1, ps1 = pack_one(angles1, '* c')
-    angles2, ps2 = pack_one(angles2, '* c')
-
-    if angles1.shape[0] == 1:
-        angles1 = angles1.expand_as(angles2)
-        ps_out = ps2
-    elif angles2.shape[0] == 1:
-        angles2 = angles2.expand_as(angles1)
-        ps_out = ps1
-    elif angles1.shape[0] == angles2.shape[0]:
-        ps_out = ps1
-    else:
-        raise ValueError('angles are not broadcastable')
-
-    comp = rot_tensor(angles1) @ rot_tensor(angles2)
-    angles_out = rot_to_euler_angles(comp)
-    return unpack_one(angles_out, ps_out, '* c')
+    xyz = comp @ torch.tensor([0, 0, 1.])
+    a, b = x_to_alpha_beta(xyz)
+    rotz = rot(0, -b, -a) @ comp
+    c = atan2(rotz[1, 0], rotz[0, 0])
+    return a, b, c
 
 def spherical_harmonics(order, alpha, beta, dtype = None):
     return get_spherical_harmonics(order, theta = (pi - beta), phi = alpha)

tests/test_irrep_repr.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch import einsum
 
 from equiformer_pytorch.spherical_harmonics import clear_spherical_harmonics_cache
-from equiformer_pytorch.irr_repr import spherical_harmonics, irr_repr, irr_repr_tensor, compose, compose_tensor
+from equiformer_pytorch.irr_repr import spherical_harmonics, irr_repr, irr_repr_tensor, compose
 from equiformer_pytorch.utils import torch_default_dtype
 
 @torch_default_dtype(torch.float64)
@@ -30,31 +30,5 @@ def test_irr_repr():
         clear_spherical_harmonics_cache()
 
         DrY = irr_repr(order, alpha, beta, gamma) @ Y
-
-        assert torch.allclose(Yrx, DrY, atol = 1e-5)
-
-@torch_default_dtype(torch.float64)
-def test_irr_repr_with_leading():
-    """
-    same as above, but with leading dimensions
-    needed to make sure one can compose batches of angles, spherical harmonics, irreps
-    """
-    for order in range(1, 7):
-        base = torch.rand(16, 3)
-        base[..., -1] = 0.
-
-        a, b, _ = base.unbind(dim = -1)
-        rotations = torch.rand(16, 3)
-        alpha, beta, gamma = rotations.unbind(dim = -1)
-
-        ra, rb, _ = compose_tensor(rotations, base).unbind(dim = -1)
-        Yrx = spherical_harmonics(order, ra, rb)
-        clear_spherical_harmonics_cache()
-
-        Y = spherical_harmonics(order, a, b)
-        clear_spherical_harmonics_cache()
-
-        rotated_irrep = irr_repr_tensor(order, rotations)
-        DrY = einsum('b m n, b n -> b m', rotated_irrep, Y)
 
         assert torch.allclose(Yrx, DrY, atol = 1e-5)