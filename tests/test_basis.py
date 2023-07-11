fc052d8451c221b520e4cd0fda66d1cb0c039c18
Phil Wang <lucidrains@gmail.com>
2023-07-11 07:12:43 -0700

    correct test

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -6,7 +6,7 @@ def test_basis():
     max_degree = 3
     x = torch.randn(2, 1024, 3)
     basis = get_basis(max_degree, device = x.device, dtype = x.dtype)
-    assert len(basis.keys()) == (max_degree + 1) ** 2, 'correct number of basis kernels'
+    assert len(basis.keys()) == max_degree ** 2, 'correct number of basis kernels'
 
 def test_basis_transformation_Q_J():
     rand_angles = torch.rand(4, 3)
